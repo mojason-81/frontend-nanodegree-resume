a136872c812204d746a256d18c2b8ca3a9b4992f
mojason <jason@mojason.com>
2015-10-12 11:06:10 -0500

    Refactor some contact data, change how contact data is displayed

--- js/helper.js
+++ js/helper.js
@@ -13,7 +13,7 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr/>';
+var HTMLheaderRole = '<p class="white-text"><span>%data%</span></p><hr/>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
@@ -28,8 +28,8 @@ var HTMLlocation = '<li class="flex-item"><span class="orange-text">Location</sp
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLskillsStart = '<!--<h3 id="skills-h3">Skills at a Glance:</h3>--><ul id="skills" class="flex-box"></ul>';
+var HTMLskills = '<li class="flex-item"><span class="orange-text">%data%</span></li>';
 
 var HTMLworkStart = '<section class="work-entry"></section>';
 var HTMLworkEmployer = '<a href="#">%data%';
