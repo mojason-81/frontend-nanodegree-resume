a43b73be02544295cad71547f7e2adaed103828c
mojason <jason@mojason.com>
2015-10-31 15:10:10 -0500

    Uncomment items for map, edit item to match .json objects in builder

--- js/helper.js
+++ js/helper.js
@@ -126,7 +126,7 @@ function initializeMap() {
     var locations = [];
 
     // adds the single location property from bio to the locations array
-    locations.push(bio.contacts.location);
+    locations.push(bio.contact.location);
 
     // iterates through school locations and appends each location to
     // the locations array
@@ -216,6 +216,7 @@ function initializeMap() {
       // function with the search results after each search.
       service.textSearch(request, callback);
     }
+    console.log(locations);
   }
 
   // Sets the boundaries of the map based on pin locations
@@ -235,11 +236,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
